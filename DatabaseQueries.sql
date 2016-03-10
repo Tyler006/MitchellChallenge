20ce10471c81c98a85353e3fec37f96f14707418
Tyler006 <tnguyendng@gmail.com>
2016-03-09 21:38:12 -0800

    Generate objects

---
+++ DatabaseQueries.sql
@@ -0,0 +1,9 @@
+﻿/*CREATE TABLE MitchellClaims(
+  ClaimNumber TEXT NOT NULL UNIQUE,
+  ClaimInfo   XML 
+);*/
+
+--DROP TABLE mitchellclaims;
+SELECT * from mitchellclaims;
+
+SELECT claiminfo FROM mitchellclaims WHERE claimnumber='22c9c23bac142856018ce14a26b6c299';
